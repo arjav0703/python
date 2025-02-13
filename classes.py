51899a9da646973659c968e067281f8a07f26e99
arjav0703 <arjavjain0703@gmail.com>
2025-02-13 19:19:53 +0530

    add classes.py

--- classes.py
+++ classes.py
@@ -2,3 +2,9 @@ class Flight():
     def __init__(self,capacity):
         self.capacity = capacity
         self.passengers = []
+
+    def addpassenger(self, name):
+        self.passengers.append(name)
+        
+    def seat_availability(self):
+        return self.capacity - len(self.passengers)
