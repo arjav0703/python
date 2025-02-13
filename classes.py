e300a755739644b7eba6e991fa0908b0938be0fa
arjav0703 <arjavjain0703@gmail.com>
2025-02-13 19:35:33 +0530

    edit classes.py

--- classes.py
+++ classes.py
@@ -3,15 +3,21 @@ class Flight():
         self.capacity = capacity
         self.passengers = []
     
-    def seat_availability(self):
-        return self.capacity - len(self.passengers)
-
+    
     def addpassenger(self, name):
-        if seat_availability() == 0:
+        if not self.seat_availability():
             return False
         self.passengers.append(name)
         return True
+    def seat_availability(self):
+        return self.capacity - len(self.passengers)
 
 pirate_air = Flight(40)
 
 ppl = ['Arjav', 'Anay', 'Satat', 'Daksh', 'Vaibhav', 'Apoorv']
+
+for callsign in ppl:
+    if pirate_air.addpassenger(callsign):
+        print(f"Added {callsign} to the flight")
+    else:
+        print(f"No available seats for {callsign}") 
